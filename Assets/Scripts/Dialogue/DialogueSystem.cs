ee19c09ab60695b2b19ea0965e20dd83f90f6fc6
gagip <y02645@google.com>
2019-06-18 21:19:17 +0900

    대화 시스템 병합

---
+++ Assets/Scripts/Dialogue/DialogueSystem.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public string txtf;
+    public Text txt;
+    public Image panel;
+
+    private int count = 0;
+    private bool isDialogue = false;
+
+    List<Dictionary<string, object>> dialogue;
+    
+    void Start()
+    {
+        dialogue = CSVReader.Read(txtf);
+    }
+
+
+    public void OnOff(bool _flag)
+    {
+        panel.gameObject.SetActive(_flag);
+        txt.gameObject.SetActive(_flag);
+    }
+
+    public void ShowDialogue()
+    {
+        OnOff(true);
+        count = 0;
+        isDialogue = true;
+        NextDialogue();
+    }
+
+    public void HideDialogue()
+    {
+        OnOff(false);
+        isDialogue = false;
+    }
+
+    private void NextDialogue()
+    {
+        txt.text = (string)dialogue[count]["dialog"];
+        if ((int) dialogue[count]["name"] == 1)
+        {
+            txt.color = Color.red;
+            txt.fontSize = 30;
+            txt.GetComponent<RectTransform>().position = new Vector3(-5, 0, 0);
+        }
+        if ((int)dialogue[count]["name"] == 2)
+        {
+            txt.color = Color.green;
+            txt.fontSize = 30;
+            txt.GetComponent<RectTransform>().position = new Vector3(-10, 0, 0);
+        }
+        if ((int)dialogue[count]["name"] == 3)
+        {
+            txt.color = Color.white;
+            txt.fontSize = 30;
+            txt.GetComponent<RectTransform>().position = new Vector3(0, 0, 0);
+        }
+        count++;
+    }
+    void Update()
+    {
+
+        if (isDialogue)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                
+                if (count < dialogue.Count)
+                {
+                    NextDialogue();
+                }
+                else
+                    HideDialogue();
+            }
+        }
+    }
+}
