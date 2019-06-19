55aed010a5714ef899cf0e7c5a1036540d64d9b9
gagip <y02645@google.com>
2019-06-19 21:25:14 +0900

    병합 중

--- Assets/Scripts/Dialogue/DialogueSystem.cs
+++ Assets/Scripts/Dialogue/DialogueSystem.cs
@@ -12,13 +12,29 @@ public class DialogueSystem : MonoBehaviour
     private int count = 0;
     private bool isDialogue = false;
 
+    public float delay = 0.1f;
+    private string currentText;
+    string txtContents;
+
     List<Dictionary<string, object>> dialogue;
     
     void Start()
     {
         dialogue = CSVReader.Read(txtf);
+
+        
     }
 
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i < txt.text.Length; i++)    // 글자 하나하나씩 출력
+        {
+            currentText = txt.text.Substring(0, i);
+            txt.text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+
+    }
 
     public void OnOff(bool _flag)
     {
@@ -43,23 +59,24 @@ public class DialogueSystem : MonoBehaviour
     private void NextDialogue()
     {
         txt.text = (string)dialogue[count]["dialog"];
+        StartCoroutine(ShowText());
         if ((int) dialogue[count]["name"] == 1)
         {
             txt.color = Color.red;
-            txt.fontSize = 30;
-            txt.GetComponent<RectTransform>().position = new Vector3(-5, 0, 0);
+            txt.fontSize = 15;
+            //txt.GetComponent<RectTransform>().position = new Vector3(-5, 0, 0);
         }
         if ((int)dialogue[count]["name"] == 2)
         {
             txt.color = Color.green;
-            txt.fontSize = 30;
-            txt.GetComponent<RectTransform>().position = new Vector3(-10, 0, 0);
+            txt.fontSize = 15;
+            //txt.GetComponent<RectTransform>().position = new Vector3(-10, 0, 0);
         }
         if ((int)dialogue[count]["name"] == 3)
         {
             txt.color = Color.white;
-            txt.fontSize = 30;
-            txt.GetComponent<RectTransform>().position = new Vector3(0, 0, 0);
+            txt.fontSize = 15;
+            //txt.GetComponent<RectTransform>().position = new Vector3(0, 0, 0);
         }
         count++;
     }
