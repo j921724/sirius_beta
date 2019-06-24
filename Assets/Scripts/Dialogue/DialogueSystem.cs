8adb9b2051ceab38a3bfa964308384e2d7912df5
gagip <y02645@google.com>
2019-06-25 01:49:17 +0900

    Scene 1 구현 (완성)
    - DialogueSystem.cs 수정
    - Scene1Test 씬 추가

--- Assets/Scripts/Dialogue/DialogueSystem.cs
+++ Assets/Scripts/Dialogue/DialogueSystem.cs
@@ -9,38 +9,43 @@ public class DialogueSystem : MonoBehaviour
     public Text txt; // 텍스트 오브젝트
     public Image panel; // 대화 중 다른 기능 금지
     public Image skipButton; // 스킵 버튼
+    public GameObject dialogueButton;  // 대화 버튼
+    [SerializeField] private float delay = 0.01f;
+    [SerializeField] private float error = 8.0f;
 
+    private GameObject player; // 플레이어 오브젝트 
     private int count = 0; // 텍스트 문서 단위
     private bool isDialogue = false; // 텍스트 UI 활성화 트리거
 
     Vector3 txtPlayer; // 플레이어 쪽 텍스트 위치
     Vector3 txtNPC; // NPC 쪽 텍스트 위치
 
-    public float delay = 0.01f;
-    string fulltext;
-
-
     List<Dictionary<string, object>> dialogue;
 
     void Start()
     {
+        if(GameObject.FindWithTag("Mary") != null)
+        {
+            player = GameObject.FindWithTag("Mary").gameObject;
+        }
         dialogue = CSVReader.Read(txtf);
     }
 
-    public void OnOff(bool _flag)
+    private void OnOff(bool _flag)
     {
         panel.gameObject.SetActive(_flag);
         skipButton.gameObject.SetActive(_flag);
         txt.gameObject.SetActive(_flag);
+        dialogueButton.SetActive(_flag);
     }
 
     public void ShowDialogue()
     {
-        SetDialoguePosition(gameObject.tag);
+        SetDialoguePosition();
         OnOff(true);
         count = 0;
-        isDialogue = true;
         NextDialogue();
+        isDialogue = true;
     }
 
     public void HideDialogue()
@@ -51,45 +56,32 @@ public class DialogueSystem : MonoBehaviour
     
     private void NextDialogue()
     {
-        fulltext = (string)dialogue[count]["dialog"];
-        
-        if ((int) dialogue[count]["name"] == 1) // 메리
-        {
-            txt.color = Color.red;
-            txt.fontSize = 14;
-            txt.GetComponent<RectTransform>().position = txtPlayer;
-            print(txt.GetComponent<RectTransform>().position);
-        }
-        if ((int)dialogue[count]["name"] == 2)
-        {
-            txt.color = Color.black;
-            txt.fontSize = 14;
-            txt.GetComponent<RectTransform>().position = txtNPC;
-            print(txt.GetComponent<RectTransform>().position);
-        }
-        StartCoroutine(ShowText());
+        string fulltext = (string)dialogue[count]["dialog"];
+
+        ChangeText();
+
+        StartCoroutine(ShowText(fulltext));
         count++;
     }
 
-    public void SetDialoguePosition(string npcName)
+    private void SetDialoguePosition()
     {
-        Vector3 v1 = new Vector3(0, 0);
-        Vector3 v2 = GameObject.FindWithTag("Mary").GetComponent<Transform>().position;
-        Vector3 v3 = GameObject.FindWithTag(npcName).GetComponent<Transform>().position;
-        
-        if (v2.x - v3.x < 0) // 플레이어가 왼쪽에 있을 시
+        Vector3 playerPos = player.transform.position; // Player 위치
+        Vector3 npcPos = transform.position; // NPC 위치
+
+        if (playerPos.x - npcPos.x < 0) // 플레이어가 왼쪽에 있을 시
         {
-            txtPlayer = new Vector3(v2.x - 3f, v2.y + 12f);
-            txtNPC = new Vector3(v3.x + 3f, v2.y + 12f);
+            txtPlayer = new Vector3(playerPos.x - 3f, playerPos.y + 12f);
+            txtNPC = new Vector3(npcPos.x + 3f, playerPos.y + 12f);
         }
         else
         {
-            txtPlayer = new Vector3(v2.x + 3f, v2.y + 12f);
-            txtNPC = new Vector3(v3.x - 3f, v2.y + 12f);
+            txtPlayer = new Vector3(playerPos.x + 3f, playerPos.y + 12f);
+            txtNPC = new Vector3(npcPos.x - 3f, playerPos.y + 12f);
         }
     }
 
-    IEnumerator ShowText()
+    IEnumerator ShowText(string fulltext)
     {
         for (int i = 0; i <= fulltext.Length; i++)    // 글자 하나하나씩 출력
         {
@@ -99,21 +91,54 @@ public class DialogueSystem : MonoBehaviour
         }
     }
 
-    void Update()
+   private void ChangeText()
     {
+        if ((int)dialogue[count]["name"] == 1) // 메리
+        {
+            txt.color = Color.red;
+            txt.GetComponent<RectTransform>().position = txtPlayer;
+        }
+        if ((int)dialogue[count]["name"] == 2) // 메들록
+        {
+            txt.color = Color.black;
+            txt.GetComponent<RectTransform>().position = txtNPC;
+        }
+    }
 
+   public void ShowButton()
+    {
+        Vector3 playerPos = player.transform.position; // Player 위치
+        Vector3 npcPos = transform.position; // NPC 위치
+
+        if (Mathf.Abs(playerPos.x - npcPos.x) < error)
+        {
+            dialogueButton.SetActive(true);
+        }
+        else
+        {
+            dialogueButton.SetActive(false);
+        }
+    }
+
+   void Update()
+    {
+        
         if (isDialogue)
         {
             if (Input.GetMouseButtonDown(0))
             {
-                
                 if (count < dialogue.Count)
                 {
                     NextDialogue();
+                    print(count);
                 }
                 else
                     HideDialogue();
             }
+        }
+        else
+        {
+            ShowButton();
         }
     }
 }
--- Assets/Scripts/test/Jiwon/Button.cs
+++ Assets/Scripts/test/Jiwon/Button.cs
@@ -5,37 +5,22 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
     private GameObject npc;
-    private GameObject target;
 
     // Start is called before the first frame update
     void Start()
     {
         npc = transform.root.gameObject;
-        if (FindObjectOfType<Controll>() != null)
-        {
-            target = FindObjectOfType<Controll>().gameObject;
-        }
-        print(target);
-        print(npc);
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
-        {
-            if (Mathf.Abs(transform.position.x - target.transform.position.x) < 4.0f)
-            {
-                gameObject.SetActive(true);
-            }
-        }
     }
 
     private void OnMouseDown()
     {
         npc.GetComponent<DialogueSystem>().ShowDialogue();
-        print("dd");
         gameObject.SetActive(false);
     }
     
