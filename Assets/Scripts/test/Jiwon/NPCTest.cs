40d9ca6f0593da60467da41b0dde6385348fad66
jiwon <21600156@handong.edu>
2019-06-28 22:50:01 +0900

    selection

--- Assets/Scripts/test/Jiwon/NPCTest.cs
+++ Assets/Scripts/test/Jiwon/NPCTest.cs
@@ -1,40 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 /*
 object와 mary가 상호작용 가능한 거리에 있을 때 상호작용 박스 생성
 */
 public class NPCTest : MonoBehaviour
 {
-    private Transform button;
+    private Transform button;   //상호작용을 위한 버튼
+    public bool isWathcing; // 메리를 쳐다보게 하는 변수
+    public bool isMoving;   // 움직이는 NPC
+
+    public float speed = 3.0f; // NPC 이동 속도
+    private Vector3 initpos; // NPC의 초기 위치
+    private Vector3 targetpos; // NPC를 이동시킬 위치
+    public float dist = 5.0f;
+    private bool direct = true; // NPC의 이동 방향
+
     private void OnTriggerStay2D(Collider2D col)    // mary가 상호작용 가능 범위 일 때
     {
         if (col.tag == "Mary")
         {
-            button.gameObject.SetActive(true);
+            if (isWathcing) // NPC가 메리를 쳐다보게 함
+            {
+                if (col.GetComponent<Transform>().position.x - 1 > gameObject.transform.position.x)
+                {
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                }
+                else if (col.GetComponent<Transform>().position.x + 1 < gameObject.transform.position.x)
+                {
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                }
+            }
         }
-
     }
-
-    private void OnTriggerExit2D(Collider2D col)    // mary가 상호작용 가능 범위 벗어날 때
-    {
-        if (col.tag == "Mary")
-        {
-            button.gameObject.SetActive(false);
-        }
-
-    }
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        button = transform.Find("button");  //자식 오브젝트 중 버튼
+        // Moving NPC를 위한 변수 정의
+        initpos.x = transform.position.x;
+        targetpos.x = initpos.x + dist;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isMoving)
+        {
+            if (EventSystem.current.IsPointerOverGameObject() == true) return; // UI창 나오면 안움직임
+
+
+            float targetDis = targetpos.x - transform.position.x; // 타겟까지의 남은 거리
+            float initDis = transform.position.x - initpos.x; // 시작 위치부터의 현위치까지의 거리
+            if (targetDis < 0 || initDis < 0)  // 원위치-타켓위치 범위를 벗어나면 방향을 바꿈
+            {
+                if (direct)
+                {
+                    direct = false;
+                }
+                else
+                {
+                    direct = true;
+                }
+            }
+            if (direct)
+            {
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+        }
 
     }
 
---
+++ Assets/Scripts/test/Jiwon/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    public Choice choice;
+    private ChoiceManager theChoice;
+    public bool flag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+        theChoice = FindObjectOfType<ChoiceManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (!flag)
+        {
+            StartCoroutine(ACoroutine());
+        }
+    }
+
+    IEnumerator ACoroutine()
+    {
+        flag = true;
+        // 캐릭터움직임 멈춤
+        theChoice.ShowChoice(choice);
+        yield return new WaitUntil(() => theChoice.choiceIng);
+        // 캐릭터 움직이게 함
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
